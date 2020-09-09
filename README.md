829dbfe1f8d037b14058b5cda034ecc53ba5e951
shunak <shuna966@gmail.com>
2020-09-09 12:12:13 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tele-work
 Create repository directly from your local folder to GitHub.
+
+## install
+At the folder you want to upload GitHub as a New Repository,
+```
+git clone https://github.com/shunak/tele-work.git
+```
+## How to use
+run (ex.case your shell is bash)
+```
+bash telework.sh "your GitHub UserName"
+```
+if input your GitHub PASSWORD, it will create New Repository
+named with current folder name so, set your directory's name
+what you want to name.
+
+Operation Checked@ bash, zsh
+
+
+
---
+++ telework.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $# != 1 ]; then
+    echo "Error:Need your GitHub User name as one argument." $*
+    exit 1
+else
+    echo "Start creating New Repository on your GitHub..."
+fi
+
+reposname=$(pwd | awk -F"/" '{print $NF}')
+
+curl -u $1 https://api.github.com/user/repos -d '{"name":"'$reposname'","description":"This Repos created from CLI."}'
+touch README.md
+echo "Now Editing..." >> README.md
+touch .gitignore
+echo "makerepos.sh" >> .gitignore
+git init
+git add .
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/$1/${reposname}.git
+git push -u origin master
+
